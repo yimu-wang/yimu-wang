6c8c9f29f20b87cb642f320a961eeb973ecf189f
yimu-wang <1476730666@qq.com>
2019-09-10 13:12:35 +0800

    first commit

---
+++ firstJAVA.java
@@ -0,0 +1,12 @@
+public class firstJAVA{	
+           public static void main(string[] args){	
+	int num=19;	
+	if(num%2 == 0){		
+	system.out.println("num是一个偶数");
+}
+else{	
+	system.out.println("num是一个奇数");		
+               }	
+          }
+}
+
