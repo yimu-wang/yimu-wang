c7de17f39ad344236aa7a08b95e162fc121a5d2d
yimu-wang <1476730666@qq.com>
2019-09-17 17:52:49 +0800

    Danli commit

---
+++ Danli.java
@@ -0,0 +1,15 @@
+
+class Single{
+        private static Single INSTANCE=new Single(); 
+        private Single(){}
+        public static Single getInstance(){
+	return INSTANCE;
+       }
+}
+class Danli{
+       public static void main(String [] args){
+	Single s1=Single.getInstance();
+	Single s2=Single.getInstance();
+	System.out.println(s1==s2);
+       }
+}
